0288ef03df3651054c53828ff6ab5e643db54598
to-the-moon-1 <darkangeloflove99@gmail.com>
2021-01-22 15:56:55 +0200

    all

--- gulpfile.js
+++ gulpfile.js
@@ -28,54 +28,53 @@ const del = require('del');
 // Определяем логику работы Browsersync
 function browsersync() {
     browserSync.init({ // Инициализация Browsersync
-        server: { baseDir: 'app/' }, // Указываем папку сервера
+        server: { baseDir: 'application/' }, // Указываем папку сервера
         notify: false, // Отключаем уведомления
         online: true // Режим работы: true или false
     })
 }
 
 function scripts() {
-    return src('app/js/**/*.js') // Берём файлы из источников
+    return src('application/js/**/*.js') // Берём файлы из источников
         .pipe(uglify()) // Сжимаем JavaScript
         .pipe(dest('dist/js/')) // Выгружаем готовый файл в папку назначения
         .pipe(browserSync.stream()) // Триггерим Browsersync для обновления страницы
 }
 
 function styles() {
-    return src('app/css/**/*.css') // Берём все файлы со стилями из папки источника
-        .pipe(concat('eshopper.min.css')) // Конкатенируем в файл app.min.js
+    return src('application/css/**/*.css') // Берём все файлы со стилями из папки источника
         .pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true })) // Создадим префиксы с помощью Autoprefixer
         .pipe(cleancss( { level: { 1: { specialComments: 0 } }/* , format: 'beautify' */ } )) // Минифицируем стили
-        .pipe(dest('app/css/')) // Выгрузим результат в папку "app/css/"
+        .pipe(dest('dist/css/')) // Выгрузим результат в папку "application/css/"
         .pipe(browserSync.stream()) // Сделаем инъекцию в браузер
 }
 
 function images() {
-    return src('app/images/**/*') // Берём все изображения из папки источника
+    return src('application/images/**/*') // Берём все изобsражения из папки источника
         .pipe(newer('dist/images/**/*')) // Проверяем, было ли изменено (сжато) изображение ранее
         .pipe(imagemin()) // Сжимаем и оптимизируем изображеня
         .pipe(dest('dist/images/')) // Выгружаем оптимизированные изображения в папку назначения
 }
 
 function cleanimg() {
-    return del('dist/images/', { force: true }) // Удаляем всё содержимое папки "app/images/dest/"
+    return del('dist/images/', { force: true }) // Удаляем всё содержимое папки "application/images/dest/"
 }
 
 function buildcopy() {
     return src([ // Выбираем нужные файлы
-        'app/css/**/*.min.css',
-        'app/js/**/*.min.js',
-        'app/images/dest/**/*',
-        'app/**/*.html',
-        'app/fonts/**/*',
-        'app/ico/**/*',
-        'app/ajax/*.php',
-        'app/.htaccess',
-        'app/robots.txt',
-        'app/manifest.json',
-        'app/service-worker.js',
-        'app/sitemap.xml',
-    ], { base: 'app' }) // Параметр "base" сохраняет структуру проекта при копировании
+        'application/css/**/*.min.css',
+        'application/js/**/*.min.js',
+        'application/images/dest/**/*',
+        'application/**/*.html',
+        'application/fonts/**/*',
+        'application/ico/**/*',
+        'application/ajax/*.php',
+        'application/.htaccess',
+        'application/robots.txt',
+        'application/manifest.json',
+        'application/service-worker.js',
+        'application/sitemap.xml',
+    ], { base: 'application' }) // Параметр "base" сохраняет структуру проекта при копировании
         .pipe(dest('dist')) // Выгружаем в папку с финальной сборкой
 }
 
@@ -86,16 +85,16 @@ function cleandist() {
 function startwatch() {
 
     // Выбираем все файлы JS в проекте, а затем исключим с суффиксом .min.js
-    watch(['app/**/*.js', '!app/**/*.min.js'], scripts);
+    watch(['applicationlication/**/*.js', '!applicationlication/**/*.min.js'], scripts);
 
     // Мониторим файлы препроцессора на изменения
-    watch('app/css/**/*', styles);
+    watch('applicationlication/css/**/*', styles);
 
     // Мониторим файлы HTML на изменения
-    watch('app/**/*.html').on('change', browserSync.reload);
+    watch('applicationlication/**/*.html').on('change', browserSync.reload);
 
     // Мониторим папку-источник изображений и выполняем images(), если есть изменения
-    watch('app/images/**/*', images);
+    watch('applicationlication/images/**/*', images);
 
 }
 
